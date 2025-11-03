66b05485023de08505e480fcc6b7db1c1a493339
Benjamin Navarro <bnavarro21@alumnos.utalca.cl>
2025-11-03 01:07:00 -0300

    Cambios en front
    front mejorado e integrado con docker

--- services/frontend/vite.config.js
+++ services/frontend/vite.config.js
@@ -5,10 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 3000,
+    port: 5173,
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || 'http://api-gateway:80',
+        target: process.env.VITE_API_URL || 'http://localhost:3000',
         changeOrigin: true,
       },
     },
